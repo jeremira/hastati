80c74bb2bd6f7086eac2f7a49d732c65280e3cda
Jeremie Miraille <jmiraille@gmail.com>
2020-09-30 11:46:08 +0200

    add validation rules for slots, #2

--- app/models/slot.rb
+++ app/models/slot.rb
@@ -2,4 +2,9 @@
 
 class Slot < ApplicationRecord
   belongs_to :user
+
+  validates :status, presence: true
+  validates :scheduled_at, presence: true
+  validates :daytime, presence: true
+  validates :max_people, presence: true, numericality: { greater_than: 0 }
 end
--- spec/models/slot_spec.rb
+++ spec/models/slot_spec.rb
@@ -5,7 +5,27 @@ require 'rails_helper'
 RSpec.describe Slot, type: :model do
   let(:slot) { build :slot }
 
-  it 'has a valid factory' do
-    expect(slot).to be_valid
+  describe 'Validation' do
+    it 'has a valid factory' do
+      expect(slot).to be_valid
+    end
+    it 'requires an user' do
+      expect(build(:slot, user: nil)).not_to be_valid
+    end
+    it 'requires a status' do
+      expect(build(:slot, status: nil)).not_to be_valid
+    end
+    it 'requires a scheduled_at timestamp' do
+      expect(build(:slot, scheduled_at: nil)).not_to be_valid
+    end
+    it 'requires a daytime timestamp' do
+      expect(build(:slot, daytime: nil)).not_to be_valid
+    end
+    it 'requires a max people amount' do
+      expect(build(:slot, max_people: nil)).not_to be_valid
+    end
+    it 'requires a max people greater than 0' do
+      expect(build(:slot, max_people: 0)).not_to be_valid
+    end
   end
 end
