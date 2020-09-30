694c9453159f841335c66b6d40f4a1cc15dc5bdf
Jeremie Miraille <jmiraille@gmail.com>
2020-09-30 12:36:45 +0200

    add slot enums, #2

--- app/models/slot.rb
+++ app/models/slot.rb
@@ -7,4 +7,7 @@ class Slot < ApplicationRecord
   validates :scheduled_at, presence: true
   validates :daytime, presence: true
   validates :max_people, presence: true, numericality: { greater_than: 0 }
+
+  enum status: {available: 0, booked: 1, payed: 2}
+  enum daytime: {lunch: 0, dinner: 1}
 end
--- spec/models/slot_spec.rb
+++ spec/models/slot_spec.rb
@@ -28,4 +28,29 @@ RSpec.describe Slot, type: :model do
       expect(build(:slot, max_people: 0)).not_to be_valid
     end
   end
+
+  describe 'Status enum' do
+    let(:slot) { create :slot }
+    it 'default to available status' do
+      expect(slot.available?).to eq true
+    end
+    it 'accept booked status' do
+      slot.booked!
+      expect(slot.booked?).to eq true
+    end
+    it 'accept payed status' do
+      slot.payed!
+      expect(slot.payed?).to eq true
+    end
+  end
+  describe 'Daytime enum' do
+    let(:slot) { create :slot }
+    it 'default to lunch daytime' do
+      expect(slot.lunch?).to eq true
+    end
+    it 'accept dinner status' do
+      slot.dinner!
+      expect(slot.dinner?).to eq true
+    end
+  end
 end
