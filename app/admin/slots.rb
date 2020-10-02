114f7f93434ee6de03bb1fd84de594260e617824
Jeremie Miraille <jmiraille@gmail.com>
2020-10-02 11:18:23 +0200

    add edit params in admin, #6

--- app/admin/slots.rb
+++ app/admin/slots.rb
@@ -2,4 +2,5 @@
 
 ActiveAdmin.register Slot do
   includes :user
+  permit_params :status, :scheduled_at, :daytime, :max_people
 end
--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,7 +2,7 @@
 
 class EventsController < ApplicationController
   before_action :authenticate_user!
-  
+
   def index
     @events = Slot.bookable.includes(:user)
   end
