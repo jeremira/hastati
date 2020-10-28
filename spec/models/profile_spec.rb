530d0ab781339fe8898567e6376523db3de90637
Jeremie Miraille <jmiraille@gmail.com>
2020-10-28 09:39:57 +0100

    add spec for profile, #14

---
+++ spec/models/profile_spec.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  let(:profile) { build :profile }
+
+  describe 'Validation' do
+    it 'has a valid factory' do
+      expect(profile).to be_valid
+    end
+  end
+
+  describe '#address' do
+    let(:tested_method) { profile.address }
+
+    context 'without any address informations' do
+      it 'returns an empty string' do
+        expect(tested_method).to eq ''
+      end
+    end
+
+    context 'with partial address informations' do
+      let(:profile) { build :profile, city: 'paris', country: 'france'}
+
+      it 'returns correct address' do
+        expect(tested_method).to eq 'paris, france'
+      end
+    end
+
+    context 'with full address informations' do
+      let(:profile) { build :profile, stree: '9 rue des mimosas', zipcode: '09140', city: 'le vernet', country: 'france'}
+
+      it 'returns correct address' do
+        expect(tested_method).to eq '9 rue des mimosas, 09140, le vernet, france'
+      end
+    end
+  end
+end
